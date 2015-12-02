c0e5cc437e383a59b7169811696595a45dda9d8c
William DeRaad <william.deraad@colorado.edu>
2015-12-01 19:25:32 -0700

    quick fix

--- lib/viz_engine.rb
+++ lib/viz_engine.rb
@@ -8,50 +8,48 @@ require 'json'
 
 include Mongo
 
-class VizEngine
 
-  configure do
-    #Location of static pages
-    config_file = File.read('package.json')
-    configs = JSON.parse config_file
-    set :public_folder, configs['config']['public_folder']
-    set :host, configs['config']['host']
-    set :port, configs['config']['port']
-    #Location of Mongo DB
-    mongo_url = configs['config']['db_url']
-    mongo_client = Mongo::Connection.from_uri(mongo_url)
-    set :db, mongo_client.db(File.basename(mongo_url))
-    disable :raise_errors, :show_exceptions,:dump_errors,:logging
-    #Static files are served before route matching
-    enable :static
-  end
+configure do
+  #Location of static pages
+  config_file = File.read('package.json')
+  configs = JSON.parse config_file
+  set :public_folder, configs['config']['public_folder']
+  set :host, configs['config']['host']
+  set :port, configs['config']['port']
+  #Location of Mongo DB
+  mongo_url = configs['config']['db_url']
+  mongo_client = Mongo::Connection.from_uri(mongo_url)
+  set :db, mongo_client.db(File.basename(mongo_url))
+  disable :raise_errors, :show_exceptions,:dump_errors,:logging
+  #Static files are served before route matching
+  enable :static
+end
 
 
 
 #Default Page
-  get '/' do
-    send_file File.join(settings.public_folder, 'index.html')
-  end
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
 
 #Summary content from Database
-  get '/summary' do
-    coll = settings.db.collection('summary')
-    content_type :json
-    if params[:location]
-      loc = params[:location]
-    else
-      loc = 'Default'
-    end
-    puts "Sending summary: #{loc}"
-    coll.find("name" => loc).to_a.first.to_json
+get '/summary' do
+  coll = settings.db.collection('summary')
+  content_type :json
+  if params[:location]
+    loc = params[:location]
+  else
+    loc = 'Default'
   end
-
-  error do
-    'Following error occured: ' + env['sinatra.error'].message
-  end
-
-  not_found do
-    'No route for request: ' + request.path
-  end
-
+  puts "Sending summary: #{loc}"
+  coll.find("name" => loc).to_a.first.to_json
 end
+
+error do
+  'Following error occured: ' + env['sinatra.error'].message
+end
+
+not_found do
+  'No route for request: ' + request.path
+end
+
