0d0004649b8e7022c93eaeed8662f88a5d9399fd
William DeRaad <william.deraad@colorado.edu>
2015-12-10 13:50:41 -0700

    update

--- log_poller.rb
+++ log_poller.rb
@@ -1,25 +1,35 @@
 require 'json'
 require_relative 'create_summary'
 
-sbuilder = CreateSummary::CreateSummary.new
-value = JSON.parse `sudo /usr/local/openvpn_as/scripts/logdba --json`
-v = value.to_a
-uname = v.last[1]
-ip = v.last[7]
+class LogPoller
+  @@sbuilder = CreateSummary::CreateSummary.new
+  def poll
+    value = JSON.parse `sudo /usr/local/openvpn_as/scripts/logdba --json`
+    v = value.to_a
+    uname = v.last[1]
+    ip = v.last[7]
 
-while true do
-  value = JSON.parse `sudo /usr/local/openvpn_as/scripts/logdba --json`
-  v = value.to_a
-  new_uname = v.last[1]
-  new_ip = v.last[7]
+    while true do
+      value = JSON.parse `sudo /usr/local/openvpn_as/scripts/logdba --json`
+      v = value.to_a
+      new_uname = v.last[1]
+      new_ip = v.last[7]
 
-  if(uname != new_uname || ip != new_ip)
-    sbuilder.setEmployeeInfo(new_uname)
-    sbuilder.setLocationInfo(new_ip, new_uname)
-    sbuilder.setPortInfo(new_ip, new_uname)
-    sbuilder.setTimeInfo(new_ip)
-    uname = new_uname
-    ip = new_ip
+
+      if(uname != new_uname || ip != new_ip)
+        @@sbuilder.setEmployeeInfo(new_uname)
+        @@sbuilder.setLocationInfo(new_ip, new_uname)
+        @@sbuilder.setPortInfo(new_ip, new_uname)
+        @@sbuilder.setTimeInfo(new_uname)
+        uname = new_uname
+        ip = new_ip
+        puts uname
+        puts ip
+      end
+      sleep(30)
+    end
   end
-  sleep(30)
 end
+
+g = LogPoller.new
+g.poll()S
